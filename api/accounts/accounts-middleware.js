1bc8d674ffaa5309c4db5b607d8f57ea423068da
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-09-07 00:40:14 -0400

    update2

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,12 +1,28 @@
+const { getById } = require("./accounts-model");
+
+exports.logger = (req, res, next) => {
+  console.log("METHOD: ", req.method);
+  next();
+};
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
-}
-
+  try {
+    const account = await getById(req.params.id);
+    if (account) {
+      req.account = account;
+      next();
+    } else {
+      next(await res.status(404).json({ message: `account not found` }));
+    }
+  } catch (error) {
+    next;
+  }
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,20 +1,26 @@
 const router = require("express").Router();
 const AccountModel = require("./accounts-model");
 
-router.get("/", async (req, res, next) => {
+const {
+  logger,
+  checkAccountPayload,
+  checkAccountNameUnique,
+  checkAccountId,
+} = require("./accounts-middleware");
+router.get("/", logger, async (req, res, next) => {
   // DO YOUR MAGIC
   const ReturnAccounts = await AccountModel.getAll();
   res.status(200).json(ReturnAccounts);
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", logger, checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
-  const ReturnAccounts = await AccountModel.getById(req.params.id);
-  res.status(200).json(ReturnAccounts);
+  res.status(200).json(req.account);
 });
 
 router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
+  
 });
 
 router.put("/:id", (req, res, next) => {
@@ -29,5 +35,4 @@ router.use((err, req, res, next) => {
   // eslint-disable-line
   // DO YOUR MAGIC
 });
-
 module.exports = router;
